e01cd80b0ea783c17ceb9dd8751375561992746d
MogtabaX <110923145+MogtabaX@users.noreply.github.com>
2023-12-02 15:22:46 +0300

    Add files via upload

---
+++ Convert the Temperature.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convert_the_Temperature
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            convertTemperature(55);
+        }
+        public static double[] convertTemperature(double celsius)
+        {
+            double[] ans = new double[2];
+            ans[0] = celsius + 273.15;
+            ans[1] = celsius * 1.80 + 32.00;
+            return ans;
+        }
+    }
+}
