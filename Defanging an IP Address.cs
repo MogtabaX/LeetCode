b309d7512616201302fa69ff16f10d120cd90806
MogtabaX <110923145+MogtabaX@users.noreply.github.com>
2023-12-11 00:26:17 +0300

    Add files via upload

---
+++ Defanging an IP Address.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Defanging_an_IP_Address
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DefangIPaddr("1.1.1.1");
+        }
+        public static string DefangIPaddr(string address)
+        {
+            string result = "";
+
+            for (int i = 0; i < address.Length; i++)
+            {
+                if (address[i] != '.')
+                {
+                    result += address[i];
+                }
+                else if (address[i] == '.')
+                {
+                    result += '[' + address[i].ToString() + ']';
+                }
+
+            }
+            return result;
+        }
+    }
+}
