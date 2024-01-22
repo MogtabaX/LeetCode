0bfd3cc1c6e935c72badc28e24403e863d0f4bb9
MogtabaX <110923145+MogtabaX@users.noreply.github.com>
2024-01-23 01:55:07 +0300

    Add files via upload

---
+++ Shuffle String.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shuffle_String
+{
+    class Program
+    {
+        public static string RestoreString(string s, int[] indices)
+        {
+            string output = "";
+            Console.WriteLine(output);
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = 0; j < s.Length; j++)
+                {
+                    if (i == indices[j])
+                    {
+                        output+=(s[j]);                            
+                    }
+                }
+            }
+            return output;
+        }
+        static void Main(string[] args)
+        {
+            RestoreString("codeleet", new int[] { 4, 5, 6, 7, 0, 2, 1, 3 });
+        }
+    }
+}
