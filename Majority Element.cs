3f07bad079513167360587eb99992b9f3ea311ec
MogtabaX <110923145+MogtabaX@users.noreply.github.com>
2024-03-07 19:44:56 +0300

    Add files via upload

---
+++ Majority Element.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Majority_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MajorityElement(new int[] { 2, 2, 1, 1, 1, 2, 2 });
+        }
+       public static int MajorityElement(int[] nums) {
+           if (nums.Length == 1)
+           {
+               return nums[0];
+           }
+           int g = nums.Length / 2;
+           HashSet<int> hash = new HashSet<int>();
+           Array.Sort(nums);
+           int x = 0;
+           for (int i = 0; i < nums.Length; i++)
+           {
+               if (!hash.Add(nums[i]))
+               {
+                   x++;
+                   if (x >= g)
+                   {
+                       return nums[i];
+                   }
+               }
+               else
+               {
+                   x = 0;
+               }
+           }
+           return 0;
+           //2024-3-7 15-20 minutes...
+    }
+    }
+}
