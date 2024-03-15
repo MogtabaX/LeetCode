19a5a6b1c0cc58a6c68c36daf8e2ef4fbc7ea4b5
MogtabaX <110923145+MogtabaX@users.noreply.github.com>
2024-03-15 20:52:51 +0300

    Add files via upload

---
+++ Add Two Integers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Add_Two_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sum(10, 10);
+        }
+        public static int Sum(int num1, int num2)
+        {
+            return ((sbyte)num1 + (sbyte)num2);  
+        }
+    }
+}
