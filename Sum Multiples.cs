bed30404d946a5c004a31c463c2bfb6438a6d2b1
MogtabaX <110923145+MogtabaX@users.noreply.github.com>
2024-03-15 22:01:22 +0300

    Add files via upload

---
+++ Sum Multiples.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_Multiples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SumOfMultiples(7);
+        }
+        public static int SumOfMultiples(int n)
+        {
+            int num = 1;
+            List<int> li = new List<int>();
+            while (num != n+1)
+            {
+                if (num % 3 == 0 || num % 5 == 0 || num % 7 == 0)
+                {
+                    li.Add(num);
+                }
+                num++;
+            }
+            int result = 0;
+            for (int i = 0; i < li.Count; ++i)
+            {
+                result += li[i];
+            }
+            return result;
+        }
+    }
+}
