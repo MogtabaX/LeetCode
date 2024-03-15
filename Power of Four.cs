004ffb235df058fa91de9cb2b1246ba7fcd0bb41
MogtabaX <110923145+MogtabaX@users.noreply.github.com>
2024-03-15 20:23:47 +0300

    10 minutes

---
+++ Power of Four.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ispowerof4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IsPowerOfFour(17);
+        }
+        public static bool IsPowerOfFour(int n)
+        {
+            if (n == 1 || n == 4)
+            {
+                return true;
+            }
+            float m = n;
+            while (m > 4)
+            {
+                m = m / 4;
+                if (m == 4)
+                { return true; }
+            }
+            return false;
+        }
+    }
+}
