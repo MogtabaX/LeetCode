bfb0ed8cfcd6991f5678c9ebcd00f6e78cccfb15
MogtabaX <110923145+MogtabaX@users.noreply.github.com>
2024-03-15 20:39:09 +0300

    Add files via upload

---
+++ Power of Three.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Power_of_Threee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IsPowerOfThree(1676909049);
+        }
+        public static bool IsPowerOfThree(int n)
+        {
+            double m = n;
+            if (n == 3 || n == 1)
+            {
+                return true;
+            }
+            while (m > 3)
+            {
+                m = m / 3;
+                if (m == 3)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
