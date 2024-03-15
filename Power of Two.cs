2af7e100c616cef964d33b1b926d88cbf97153e1
MogtabaX <110923145+MogtabaX@users.noreply.github.com>
2024-03-15 20:42:21 +0300

    Add files via upload

---
+++ Power of Two.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Power_of_Two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IsPowerOfTwo(16);
+        }
+        public static bool IsPowerOfTwo(int n)
+        {
+            double m = n;
+            if (n == 1 || n == 2)
+            {
+                return true;
+            }
+            while (m > 2)
+            {
+                m = m / 2;
+                if (m == 2)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
