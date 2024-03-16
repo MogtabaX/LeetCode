a8e1f17372b27a97eba47fc24f0564797dbf405b
MogtabaX <110923145+MogtabaX@users.noreply.github.com>
2024-03-16 05:26:06 +0300

    Add files via upload

---
+++ Running Sum of 1d Array.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Running_Sum_of_1d_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunningSum(new int[] { 1, 2, 3, 4 });
+        }
+
+        public static int[] RunningSum(int[] nums)
+        {
+            int[] result = new int[nums.Length];
+            int x=0;
+            for(int i=0;i<nums.Length;++i)
+            {
+                x+=nums[i];
+                result[i]=x;
+            }
+            return result;
+        }
+    }
+}
