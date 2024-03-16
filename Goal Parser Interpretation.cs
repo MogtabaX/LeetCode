9543f2a19bead497878fbf27292a421321074cfd
MogtabaX <110923145+MogtabaX@users.noreply.github.com>
2024-03-16 23:22:51 +0300

    Add files via upload

---
+++ Goal Parser Interpretation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goal_Parser_Interpretation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Interpret("(al)G(al)()()G");
+        }
+        public static string Interpret(string command)
+        {
+            StringBuilder x = new StringBuilder();
+            int length = command.Length;
+            if (command[0] == 'G')
+            {
+                x.Append('G');
+            }
+            for (int i = 1; i < length; ++i)
+            {
+                if (command[i] == 'G')
+                {
+                    x.Append('G');
+                }
+
+                if (command[i - 1] == 'a' && command[i] == 'l')
+                {
+                    x.Append("al");
+                    continue;
+                }
+                if (command[i - 1] == '(' && command[i] == ')')
+                {
+                    x.Append('o');
+                    continue;
+                }
+
+            }
+            return x.ToString();
+        }
+    }
+}
