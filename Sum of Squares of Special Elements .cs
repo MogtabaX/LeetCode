7c16bb843a9d8e00d4ae34c162bc52197a6cbfad
MogtabaX <110923145+MogtabaX@users.noreply.github.com>
2024-03-16 22:45:50 +0300

    Add files via upload

---
+++ Sum of Squares of Special Elements .cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_of_Squares_of_Special_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SumOfSquares(new int[] { 1, 2, 3, 4 });
+        }
+        public static int SumOfSquares(int[] nums)
+        {
+            sbyte i = 0;
+            sbyte length =(sbyte) nums.Length;
+            int x = 0;
+            
+            for (i = 0; i < length; ++i)
+            {
+                if (length % (i+1) == 0)
+                {
+                    x += nums[i]*nums[i];
+                }
+            }
+            return x;
+        }
+    }
+}
