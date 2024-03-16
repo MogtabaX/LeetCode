1cf66abe179e56de3522a3a365b1d8e7a786d8d5
MogtabaX <110923145+MogtabaX@users.noreply.github.com>
2024-03-16 22:10:57 +0300

    Add files via upload

---
+++ Check if the Sentence Is Pangram.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_if_the_Sentence_Is_Pangram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckIfPangram("thequickbrownfoxjumpsoverthelazydog");
+        }
+        public static bool CheckIfPangram(string sentence)
+        {
+            string alpha = "abcdefghijklmnopqrstuvwxyz";
+            HashSet<char> checker = new HashSet<char>();
+            if (sentence.Length < alpha.Length)
+            {
+                return false;
+            }
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                checker.Add(sentence[i]);
+            }
+            char[] sent = checker.ToArray();
+            if (sent.Length == alpha.Length)
+            {
+                return true;
+            }
+            return false;  
+        }
+    }
+}
