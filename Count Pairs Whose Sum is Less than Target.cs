b5ac1162aba600f6df9aa0b63a2b6d3948592510
MogtabaX <110923145+MogtabaX@users.noreply.github.com>
2024-03-18 04:39:38 +0300

    Add files via upload

---
+++ Count Pairs Whose Sum is Less than Target.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count_Pairs_Whose_Sum_is_Less_than_Target
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CountPairs(new int[] { -1, 1, 2, 3, 1 }, 2);
+        }
+        public static int CountPairs(IList<int> nums, int target)
+        {
+            int n= nums.Count;
+            int x = 0;
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = 0; j < n; ++j)
+                {
+                    if (((n > j && j > i)) && nums[i] + nums[j] < target)
+                    {
+                        x++;
+                    }
+                }
+            }
+            return x;
+        }
+    }
+}
