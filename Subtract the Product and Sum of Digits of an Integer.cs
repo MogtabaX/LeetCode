be0b05ed3b67a999cd08e5144fb7b58140dab61c
MogtabaX <110923145+MogtabaX@users.noreply.github.com>
2024-03-21 06:01:47 +0300

    Add files via upload

---
+++ Subtract the Product and Sum of Digits of an Integer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subtract_the_Product_and_Sum_of_Digits_of_an_Integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SubtractProductAndSum(234);
+        }
+        public static int SubtractProductAndSum(int n)
+        {
+            int x = 1;
+            int y = 0;
+            string b = n.ToString();
+            for (int i = 0; i < b.Length; ++i)
+            {
+                x *= (int)char.GetNumericValue(b[i]);
+            }
+            for (int i = 0; i < b.Length; ++i)
+            {
+                y+= (int)char.GetNumericValue(b[i]);
+            }
+            return x - y;
+        }
+    }
+}
