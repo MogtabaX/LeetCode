aa41e32d67c158d5a7b224b0fb7c3cdad709122d
MogtabaX <110923145+MogtabaX@users.noreply.github.com>
2024-03-21 22:49:37 +0300

    Add files via upload

---
+++ Check if a String Is an Acronym of Words.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_if_a_String_Is_an_Acronym_of_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static bool IsAcronym(IList<string> words, string s)
+        {
+            StringBuilder acronym = new StringBuilder();
+            for (int i = 0; i < words.Count; ++i)
+            {
+                acronym.Append(words[i].ElementAt(0));
+            }
+            if (acronym.ToString() == s)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
