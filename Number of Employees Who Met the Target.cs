de1a86fc97796640b5392633426dc9e6a72dd4f1
MogtabaX <110923145+MogtabaX@users.noreply.github.com>
2024-03-21 05:52:05 +0300

    Add files via upload

---
+++ Number of Employees Who Met the Target.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_of_Employees_Who_Met_the_Target
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static int NumberOfEmployeesWhoMetTarget(int[] hours, int target)
+        {
+            int x = 0;
+            int l = hours.Length;
+
+            for (int i = 0; i < l; ++i)
+            {
+                if (hours[i] >= target)
+                {
+                    ++x;
+                }
+            }
+            return x;
+        }
+    }
+}
