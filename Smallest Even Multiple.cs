fdc13d7c99eca716db9ba1d5fc64589ea94030ff
MogtabaX <110923145+MogtabaX@users.noreply.github.com>
2024-03-21 05:56:18 +0300

    Add files via upload

---
+++ Smallest Even Multiple.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smallest_Even_Multiple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static int SmallestEvenMultiple(int n)
+        {
+            int max = 2 * n + 1;
+            int x = 0;
+            int i = 1;
+            while (i < max)
+            {
+                if (i % n == 0 && i % 2 == 0)
+                {
+                    return i;
+                }
+                ++i;
+
+            }
+            return 0;
+        }
+    }
+}
