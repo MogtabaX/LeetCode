21f0ba2d2926cb1d80f3d9afb740933398a53060
MogtabaX <110923145+MogtabaX@users.noreply.github.com>
2024-03-23 01:27:43 +0300

    stack at it again !

---
+++ Removing Stars From a String.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Removing_Stars_From_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RemoveStars("a*bc**d*dy");
+        }
+        public static string RemoveStars(string s)
+        {
+            Stack<char> sta = new Stack<char>();
+            Stack<char> stak = new Stack<char>();
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < s.Length; ++i)
+            {
+                if (s[i] == '*')
+                {
+                    sta.Pop();
+                }
+                else
+                {
+                    sta.Push(s[i]);
+                }
+            }
+            if (sta.Count == s.Length)
+            {
+                return s;
+            }
+            char[] a = new char[sta.Count];
+            while (sta.Count > 0)
+            {
+                stak.Push(sta.Pop());
+            }
+            while (stak.Count > 0)
+            {
+                result.Append(stak.Pop());
+            }
+            return result.ToString();
+           
+        }
+    }
+}
