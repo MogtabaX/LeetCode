de6182bbc0d87ddb1e8e37442cbfeb7d42be4860
MogtabaX <110923145+MogtabaX@users.noreply.github.com>
2024-07-05 19:27:35 +0300

    Add files via upload

---
+++ Richest Customer Wealth.cs
@@ -0,0 +1,23 @@
+﻿public class Solution
+{
+    public int MaximumWealth(int[][] accounts)
+    {
+        int x = 0;
+        int y = 0;
+        for (int i = 0; i < accounts.Length; ++i)
+        {
+            for (int j = 0; j < accounts[i].Length; ++j)
+            {
+                y += accounts[i].ElementAt(j);
+            }
+            if (y > x)
+            {
+                x = y;
+                y = 0;
+            }
+            y = 0;
+        }
+
+        return x;
+    }
+}
