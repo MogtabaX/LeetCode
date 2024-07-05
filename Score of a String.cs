95f75ab06c1b47ffe29d1d2f8e0a150d38cb970a
MogtabaX <110923145+MogtabaX@users.noreply.github.com>
2024-07-05 19:28:31 +0300

    Add files via upload

---
+++ Score of a String.cs
@@ -0,0 +1,16 @@
+﻿class Solution
+{
+    static void Main()
+    {
+        scoreOfString("zaz");
+    }
+    public static int scoreOfString(String s)
+    {
+        int x = 0;
+        for (int i = 0; i < s.Length-1; ++i)
+        {
+            x += Math.Abs((int)(s[i] - s[i + 1]));
+        }
+        return x;
+    }
+}
